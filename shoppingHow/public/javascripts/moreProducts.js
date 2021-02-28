e396872f0278c8601881af0e83d546cdde76a2b2
Jungwon Lee <leejungwon96@gmail.com>
2021-02-28 16:24:02 +0900

    [refactor] moreProduct.js functions into a Class

--- shoppingHow/public/javascripts/moreProducts.js
+++ shoppingHow/public/javascripts/moreProducts.js
@@ -1,46 +1,53 @@
-let rawData = {};
-function loadItems() {
-  return fetch("http://localhost:3000/homeContents.json").then((data) =>
-    data.json()
-  );
-}
-
-function getHtmlWithData(datas, start, end) {
-  let string = "";
-  for (let i = start; i < end; i++) {
-    string += `<li>
-      <img src="${datas[i].eventContent.imgurl}"></img>
-      <span class="ellipsis">${datas[i].eventContent.title}</span>
-      <span>${datas[i].eventContent.subtitle}</span>
-      <span class="span_theme">테마</span>
-      </li>`;
+class MoreProducts {
+  constructor() {
+    //
+    this.rawData = {};
+    this.seeMoreBtn = document.querySelector(".section_products_seeMore");
+    this.addEvents();
   }
-  return string;
-}
 
-function makeHTML() {
-  loadItems().then((v) => {
-    document.querySelector(
-      ".section_products_list"
-    ).innerHTML = getHtmlWithData(v.contents, 0, 5);
-    rawData = v.contents;
-  });
-}
-
-const seeMoreBtn = document.querySelector(".section_products_seeMore");
-
-seeMoreBtn.addEventListener("click", () => {
-  if (seeMoreBtn.innerHTML === "더보기 ▼") {
-    document.querySelector(
-      ".section_products_list"
-    ).innerHTML += getHtmlWithData(rawData, 5, 10);
-    seeMoreBtn.innerHTML = "접기 ▲";
-  } else if (seeMoreBtn.innerHTML === "접기 ▲") {
-    document.querySelector(
-      ".section_products_list"
-    ).innerHTML = getHtmlWithData(rawData, 0, 5);
-    seeMoreBtn.innerHTML = "더보기 ▼";
+  loadItems() {
+    return fetch("http://localhost:3000/homeContents.json").then((data) =>
+      data.json()
+    );
   }
-});
+  getHtmlWithData(datas, start, end) {
+    let string = "";
+    for (let i = start; i < end; i++) {
+      string += `<li>
+        <img src="${datas[i].eventContent.imgurl}"></img>
+        <span class="ellipsis">${datas[i].eventContent.title}</span>
+        <span>${datas[i].eventContent.subtitle}</span>
+        <span class="span_theme">테마</span>
+        </li>`;
+    }
+    return string;
+  }
 
-makeHTML();
+  renderHTML() {
+    this.loadItems().then((v) => {
+      document.querySelector(
+        ".section_products_list"
+      ).innerHTML = this.getHtmlWithData(v.contents, 0, 5);
+      this.rawData = v.contents;
+    });
+  }
+  addEvents() {
+    this.seeMoreBtn.addEventListener("click", () => {
+      if (this.seeMoreBtn.innerHTML === "더보기 ▼") {
+        document.querySelector(
+          ".section_products_list"
+        ).innerHTML += this.getHtmlWithData(this.rawData, 5, 10);
+        this.seeMoreBtn.innerHTML = "접기 ▲";
+      } else if (this.seeMoreBtn.innerHTML === "접기 ▲") {
+        document.querySelector(
+          ".section_products_list"
+        ).innerHTML = this.getHtmlWithData(this.rawData, 0, 5);
+        this.seeMoreBtn.innerHTML = "더보기 ▼";
+      }
+    });
+  }
+}
+
+const mBtn = new MoreProducts();
+mBtn.renderHTML();
